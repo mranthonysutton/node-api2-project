ac772daf82238c1442fbafcc4d0ddc1d1419c597
Anthony Sutton <anthony.sutton95@gmail.com>
2019-12-03 12:35:00 -0800

    Completed all GET requests

--- api/blog-routes.js
+++ api/blog-routes.js
@@ -4,8 +4,58 @@ const Posts = require("../data/db");
 
 router.use(express.json());
 
+// Returns all blog posts
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "It's working!" });
+  Posts.find()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+// gets a specific blog post
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then(post => {
+      if (post.length !== 0) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+// Finds all comments associated with a blog post
+router.get("/:id/comments", (req, res) => {
+  Posts.findPostComments(req.params.id)
+    .then(comments => {
+      if (comments.length !== 0) {
+        res.status(200).json(comments);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
+    });
 });
 
 module.exports = router;
